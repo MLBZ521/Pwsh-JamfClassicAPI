6df052ea98156ffb83991b62f4e8ba05b5a6984d
Zack T <MLBZ521@gmail.com>
2019-01-11 15:51:28 -0700

    Renamed Parameter to resolve issue with partial name mapping
    + Renamed Parameter to resolve issue with partial name mapping
    + Minor format improvements

--- JamfClassicAPI/Public/Invoke-JamfClassicAPI.ps1
+++ JamfClassicAPI/Public/Invoke-JamfClassicAPI.ps1
@@ -20,41 +20,38 @@
 function Invoke-JamfClassicAPI() {
     [CmdletBinding()]
     Param(
-        [Parameter(ParameterSetName='Authentication')]
+        [Parameter(ParameterSetName = 'Authentication')]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.CredentialAttribute()]
         $Authentication = $script:APICredentials,
 
-        [ValidateSet('GET','PUT','POST','DELETE', IgnoreCase = $true)]
+        [ValidateSet('GET', 'PUT', 'POST', 'DELETE', IgnoreCase = $true)]
         [string]$Method = 'GET',
 
-        [ValidateSet('xml','json', IgnoreCase = $true)]
+        [ValidateSet('xml', 'json', IgnoreCase = $true)]
         [string]$Header = 'xml',
 
+        [hashtable]$Endpoints,
+        
         [psobject]$Body,
-
-        [hashtable]$ResourceParameters,
 
         [string]$Server = $env:JamfProServer
     )
 
     DynamicParam {
+        # Build Dynamic Paramter for $Resource
         $RuntimeParamDict = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
         $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-
-        # Build Dynamic Paramter for $Resource
         $ParameterAttributes = New-Object System.Management.Automation.ParameterAttribute
         $ParameterAttributes.HelpMessage = "Provide the Resource to access:"
-        $ParameterAttributes.Mandatory = $Mandatory.IsPresent
+        $ParameterAttributes.Mandatory = $true
         $ParameterAttributes.ParameterSetName = '__AllParameterSets'
-        $ParameterAttributes.ValueFromPipeline = $ValueFromPipeline.IsPresent
-        $ParameterAttributes.ValueFromPipelineByPropertyName = $ValueFromPipelineByPropertyName.IsPresent
+        $ParameterAttributes.ValueFromPipeline = $true
+        $ParameterAttributes.ValueFromPipelineByPropertyName = $true
         $AttributeCollection.Add($ParameterAttributes)
-
         $AttributeCollection.Add((New-Object System.Management.Automation.ValidateSetAttribute( $( $global:APIResources.Path ) )))
         $RuntimeParam = New-Object System.Management.Automation.RuntimeDefinedParameter('Resource', [string], $AttributeCollection)
         $RuntimeParamDict.Add('Resource', $RuntimeParam)
-
         return $RuntimeParamDict
     }
 
@@ -64,10 +61,10 @@ function Invoke-JamfClassicAPI() {
         # Set the proper Header content based on the provided parameter value.
         switch ($Header) {
             "xml" {
-                [psobject]$Header = @{"accept"="application/xml"}
+                [psobject]$Header = @{"accept" = "application/xml"}
             }
             "json" {
-                [psobject]$Header = @{"accept"="application/json"}
+                [psobject]$Header = @{"accept" = "application/json"}
             }
         }
         
@@ -76,33 +73,34 @@ function Invoke-JamfClassicAPI() {
             Write-Error -Message "The selected resource does not support the provided method." -ErrorAction Stop
         }
 
-        # Check if a $ResourceParamter is required for the requested $Resource and fail out if a $ResourceParamter was not provided.
-        if ( $( $Resource -split "/" | Where-Object { $_ -match "[{].+[}]" } ).Count -ne 0 -and $null -eq $ResourceParamter ) {
-            Write-Error -Message "The selected resource requires a `$ResourceParamter to be provided." -ErrorAction Stop
+        # Check if a $Endpoints is required for the requested $Resource and fail out if a $Endpoints was not provided.
+        if ( $( $Resource -split "/" | Where-Object { $_ -match "[{].+[}]" } ).Count -ne 0 -and $null -eq $Endpoints ) {
+            Write-Error -Message "The selected resource requires a `$Endpoints to be provided." -ErrorAction Stop
         }
-        # If a $ResourceParamter is required for the requested $Resource, replace the provide $ResourceParamter in the $Resource string.
+        # If a $Endpoints is required for the requested $Resource, replace the provide $Endpoints in the $Resource string.
         elseif ( $( $Resource -split "/" | Where-Object { $_ -match "[{].+[}]" } ).Count -gt 0 ) {
             Write-Verbose -Message "Updating the Resourse with the provided Parameters"
 
-            ForEach ( $Parameter in $ResourceParameters.Keys ) {
-                $Resource = $( $Resource | Where-Object { $_ -match "[{].+[}]" } ) -replace "[{]$Parameter[}]", $ResourceParameters[$Parameter]
+            ForEach ( $Endpoint in $Endpoints.Keys ) {
+                $Resource = $( $Resource | Where-Object { $_ -match "[{].+[}]" } ) -replace "[{]$Endpoint[}]", $Endpoints[$Endpoint]
             }
         }
         
-        Write-Verbose "Invoke method `"${Method}`" on resource `"${Resource}`" with header `"accept: $(${Header}.Values)`""
+        Write-Verbose -Message "Invoke method `"${Method}`" on resource `"${Resource}`" with header `"accept: $(${Header}.Values)`""
         $Uri = "${Server}/JSSResource${Resource}"
     }
 
     Process {
-        Try {
+        try {
             $response = Invoke-RestMethod -Uri "${Uri}" -Method $Method -Headers $Header -Credential $Authentication -ErrorVariable RestError -ErrorAction SilentlyContinue
         }
-        Catch {
+        catch {
             $statusCode = $_.Exception.Response.StatusCode.value__
 
-            If ($statusCode -notcontains "200") {
+            if ($statusCode -notcontains "200") {
                 $errorDescription = $($RestError.Message -split [Environment]::NewLine)
                 Write-Host -Message "FAILED:  ${statusCode} / $($errorDescription[5]) - $($errorDescription[6])"
+                # Write-Host -Message "FAILED:  ${statusCode} / $($RestError.Message)"
             }
         }
         return $response

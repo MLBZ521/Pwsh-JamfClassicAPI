5763e61debd14a1b09122e7e49f63c44c78040ca
Zack T <MLBZ521@gmail.com>
2019-01-10 21:46:26 -0700

    Added logic to "setup" the environment on import of module
    + Added some logic to "setup" the environment on import of module (currently debating on using this)

--- JamfClassicAPI/JamfClassicAPI.psm1
+++ JamfClassicAPI/JamfClassicAPI.psm1
@@ -20,3 +20,19 @@ foreach ( $Folder in @( 'Private', 'Public' ) ) {
 
 Export-ModuleMember -Function ( Get-ChildItem -Path "${PSScriptRoot}\Public\*.ps1" ).BaseName
 # Export-ModuleMember -Function ( Get-ChildItem -Path "${PSScriptRoot}\Private\*.ps1" ).BaseName
+
+##################################################
+# Run after loading the module
+
+$URL = $( Read-Host "Please specify the Jamf Pro Server URL (https://jamf.company.com:8443)" )
+$Save = $( Read-Host "Would you like to permanently save this to you your user environment for furture use?" )
+
+# Set the JPS Server URL in the PowerShell Environment
+Set-JamfServer -Url "${URL}" -Save "${Save}"
+
+# Set the session to use TLS 1.2
+Write-Host "Setting session Security Protocol to TLS 1.2"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Get available API resource endpoints from the JPS
+$global:APIResources = Get-JamfAPIResources -Server "${URL}"

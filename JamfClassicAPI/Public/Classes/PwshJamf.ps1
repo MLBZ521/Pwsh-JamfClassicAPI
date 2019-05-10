c5c6f31b37ff93c12afff68e7cd444f0e774a2cd
Zack T <MLBZ521@gmail.com>
2019-05-10 16:21:41 -0700

    Added full support for /computerapplications
    Added full support for the /computerapplications API Resource.

--- JamfClassicAPI/Public/Classes/PwshJamf.ps1
+++ JamfClassicAPI/Public/Classes/PwshJamf.ps1
@@ -663,6 +663,41 @@ Class PwshJamf {
     }
 
 
+    ##### Resource Path:  /computerapplications #####
+
+    # Returns computerapplication by name
+    [psobject] GetComputerApplicationByName($Application) {
+        $Resource = "computerapplications/application/${Application}"
+        $Method = "GET"
+        $Results = $this.InvokeAPI($Resource, $Method)
+        return $Results
+    }
+
+    # Returns computerapplication by name, including inventory information
+    [psobject] GetComputerApplicationByNameAndInventory($Application, $Inventory) {
+        $Resource = "computerapplications/application/${Application}/inventory/${Inventory}"
+        $Method = "GET"
+        $Results = $this.InvokeAPI($Resource, $Method)
+        return $Results
+    }
+
+    # Returns computerapplication by name and version
+    [psobject] GetComputerApplicationByNameAndVersion($Application, $Version) {
+        $Resource = "computerapplications/application/${Application}/version/${Version}"
+        $Method = "GET"
+        $Results = $this.InvokeAPI($Resource, $Method)
+        return $Results
+    }
+
+    # Returns computerapplication by name and version, including inventory information
+    [psobject] GetComputerApplicationByNameAndVersionAndInventory($Application, $Version, $Inventory) {
+        $Resource = "computerapplications/application/${Application}/version/${Version}/inventory/${Inventory}"
+        $Method = "GET"
+        $Results = $this.InvokeAPI($Resource, $Method)
+        return $Results
+    }
+
+
     ##### Resource Path:  /categories #####
 
     # Returns all categories
